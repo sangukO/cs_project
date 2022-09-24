43458a0817e49bbf2c217fd7907961261aa8b63c
sangukO <woqls1972@gmail.com>
2022-09-24 23:14:03 +0900

    Nav commit 9/24

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,10 @@
 import {Link} from "react-router-dom"
+import Nav from "./Nav"
 
 function Home() {
     return (
         <div>
-            <div className="Nav" style={{display:'flex', justifyContent:'flex-end'}}>
-                <div style={{}}><h2><Link to="/">Home</Link></h2></div>
-                <div><h2><Link to="/Login">Login</Link></h2></div>
-                <div style={{marginLeft:'30px', marginRight:'10px'}}><h2><Link to="/Join">Join</Link></h2></div>
-            </div>
+            <div className="NavDiv"><Nav/></div>
             <h1>편의점 통합 관리 웹앱</h1>
         </div>
     )
