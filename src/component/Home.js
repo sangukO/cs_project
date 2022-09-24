7ea1f86d7e91e19b6c3878c283fa4e2305efa18d
khyunho <smacaron@naver.com>
2022-09-25 00:02:49 +0900

    commit test 9/25

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,17 +4,8 @@ import Nav from "./Nav"
 function Home() {
     return (
         <div>
-<<<<<<< HEAD
-            <div className="Nav" style={{display:'flex', justifyContent:'flex-end'}}>
-                <div style={{}}><h2><Link to="/">Home</Link></h2></div>
-                <div><h2><Link to="/Login">Login</Link></h2></div>
-                <div style={{marginLeft:'30px', marginRight:'10px'}}><h2><Link to="/Join">Join</Link></h2></div>
-            </div>
-            <h1>편의점 통합 관리 웹앱 commit test</h1>
-=======
             <div className="NavDiv"><Nav/></div>
             <h1>편의점 통합 관리 웹앱 change</h1>
->>>>>>> main
         </div>
     )
 }
