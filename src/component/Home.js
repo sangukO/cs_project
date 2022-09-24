c9c6eca709b9e45fc7dbbb6c3d73f58e901a986c
khyunho <smacaron@naver.com>
2022-09-24 22:58:33 +0900

    commit test 9/24

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,7 +8,7 @@ function Home() {
                 <div><h2><Link to="/Login">Login</Link></h2></div>
                 <div style={{marginLeft:'30px', marginRight:'10px'}}><h2><Link to="/Join">Join</Link></h2></div>
             </div>
-            <h1>편의점 통합 관리 웹앱</h1>
+            <h1>편의점 통합 관리 웹앱 commit test</h1>
         </div>
     )
 }
