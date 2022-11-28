9ebd063a972dd6cc4358c9d6897721542b819ea8
sangukO <71215834+sangukO@users.noreply.github.com>
2022-11-29 00:15:23 +0900

    Feature/add memo crud (#20)
    * docs: update directory
    
    * feat: add MemoModal
    
    * feat: todo-update
    
    * feat: updateTodo and deleteTodo
    
    * Create README.md
    
    * feat: Add TodoEditFormDate
    
    * docs: Update directory
    
    * docs: fix: typo
    
    * feat: Add staff list and chart
    
    * feat: Add notice and write pages and fix nav link
    
    * feat: Add notice CR page without DB
    
    * feat: Add EditNoticePage
    
    * docs: Update css
    
    * feat: Add todo, staff, chart CRUD
    
    * feat: Add memo CRUD
    
    * feat: Add memo, chart and notice CRUD
    
    * fix: memo modal
    
    * fix: fix chart color
    
    * docs: Fix comments
    
    * docs: Remove console.log() and alert message
    
    * Fix: cookie
    
    * fix: Add dev.js, .gitignore
    
    * feat: localstorage 방식 로그인 유지 기능 추가, 작성 모달 달력 추가
    
    * feat: 할 일 작성 모달 형식 수정

--- cs_project/server/index.js
+++ cs_project/server/index.js
@@ -7,6 +7,9 @@ const port = 3001;
 
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
 const { User } = require("./models/User")
 const { auth } = require("./middleware/auth")
 const { Board } = require("./models/Board")
@@ -20,7 +23,7 @@ app.use(cookieParser());
 
 //몽고디비 연결
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://root:complicatedpassword@csproject.vjvtipf.mongodb.net/cs_project", {
+mongoose.connect(config.mongoURI, {
   // useNewUrlParser: true, 
   // useUnifiedTopology: true, 
   // useCreateIndex: true, 
@@ -74,13 +77,12 @@ app.post('/api/login', (req, res) => {
         // x-auth-token : 엔드 포인트에서 인증 토큰 요청해서 
         // 성공시 서버에 인증 토큰을 반환해 줌
         // 그 토큰을 쿠키에 저장
-        console.log(user.token)
+
         res.cookie('x_auth', user.token)
           .status(200)
           .json({
             loginSuccess: true,
-            _id: user._id,
-            userId: user.userId
+            userInfo: user
           });
         //console.log(res.cookie.x_auth)
         // console.log(user._id)
@@ -109,6 +111,7 @@ app.get('/auth', auth, (req, res) => {
 app.post('/api/logout', (req, res) => {
   User.findOneAndUpdate({ userId: req.body.userId }, { token: "" }, (err, user) => {
     if (err) return res.json({ success: false, err });
+    res.clearCookie('x_auth');
     return res.status(200).send({
       success: true
     })
